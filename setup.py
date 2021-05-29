8e0b1227c51f522444ac6089cb4a98851d1718aa
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-05-29 09:42:35 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup  # type: ignore
+from setuptools import find_packages
+
+
+if __name__ == '__main__':
+    # This is based on the instructions from
+    # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+    from os import path
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_file:
+        long_description = readme_file.read()
+
+    setup(
+        name='identifycompiler',
+        version='1.0.0',
+        url='https://github.com/yugabyte/identifycompiler',
+        author='Mikhail Bautin',
+        author_email='mbautin@users.noreply.github.com',
+        description='Identifying the properties of a C/C++ compiler, such as type and version',
+        packages=find_packages(),
+        install_requires=[],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        extras_require={
+            # Following advice in this answer: https://stackoverflow.com/a/28842733/220215
+            # Install with: pip install -e '.[dev]'
+            'dev': [
+                'pycodestyle',
+                'mypy',
+                'twine',
+                'codecheck'
+            ]
+        }
+    )
